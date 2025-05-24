987ef5a4a657ebe3daf0776e6d2f397e90b17d65
Cristian Trif <cristiantrif29@gmail.com>
2025-05-24 16:13:43 +0300

    Update for GitHub Pages deployment with HashRouter

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,12 +10,12 @@ export default function Homepage() {
         <h1>
           You travel the world.
           <br />
-          WorldWise keeps track of your adventures.
+          Travelapp keeps track of your adventures.
         </h1>
         <h2>
-          A world map that tracks your footsteps into every city you can think
-          of. Never forget your wonderful experiences, and show your friends how
-          you have wandered the world.
+          Track your footsteps into every city you can think of. Never forget
+          your wonderful experiences, and show your friends how you have
+          wandered the world.
         </h2>
         <Link to="/login" className="cta">
           Start tracking now
